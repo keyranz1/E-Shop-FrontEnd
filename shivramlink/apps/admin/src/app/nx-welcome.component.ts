be8c8ad7650eb88a6a5d1e5ab8aa2d653d95b723
kiranyadubanshi <kiran.yadubanshi@gmail.com>
2023-01-13 22:53:01 -0600

    initial admin app commit

--- shivramlink/apps/admin/src/app/nx-welcome.component.ts
+++ shivramlink/apps/admin/src/app/nx-welcome.component.ts
@@ -433,7 +433,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome enduser 👋
+            Welcome admin 👋
           </h1>
         </div>
 
