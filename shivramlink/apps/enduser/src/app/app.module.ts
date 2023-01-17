8a3e694c5eb69f6f973eedb7e5140ef990bd8784
kiranyadubanshi <kiran.yadubanshi@gmail.com>
2023-01-16 23:59:28 -0600

    header and footer initial component

--- shivramlink/apps/enduser/src/app/app.module.ts
+++ shivramlink/apps/enduser/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent},
-  { path: 'products', component: ProductListComponent}
-]
-
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
